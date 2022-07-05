3f41f3370a19ab7c168a21a25fb376491dae3525
Kyle KIM <k2wackie@gmail.com>
2022-07-05 14:30:06 +0900

    vue SSR 적용

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import '@babel/polyfill'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import Vue from "vue";
+import App from "./App.vue";
+import { createRouter } from "./router";
+import { createStore } from "./store";
+import { sync } from "vuex-router-sync";
+import vuetify from "./plugins/vuetify";
+import "@babel/polyfill";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+export function createApp(ctx) {
+  const router = createRouter();
+  const store = createStore();
+  sync(store, router);
+
+  const app = new Vue({
+    data: { url: ctx ? ctx.url : "" },
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+  });
+
+  return { app, router, store };
+}
