f9208f07f91b6d74e68bd4ee0bad78a9a5d465df
Kyle KIM <k2wackie@gmail.com>
2022-07-05 16:28:22 +0900

    배포 테스트

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,13 @@
 <template>
-  <hello-world />
+  <div>배포테스트</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
+  name: "Home",
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    HelloWorld,
+  },
+};
 </script>
